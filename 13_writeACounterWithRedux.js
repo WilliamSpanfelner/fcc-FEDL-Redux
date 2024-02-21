ffa55aa5dd38a24aff552fc88e226603af2f34b6
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-02-21 17:52:11 +0200

    Create a switch block to update state depending on action.type received

--- 13_writeACounterWithRedux.js
+++ 13_writeACounterWithRedux.js
@@ -3,7 +3,14 @@ const INCREMENT = 'INCREMENT'; // Define a constant for increment action types
 const DECREMENT = 'DECREMENT'; // Define a constant for increment action types
 
 const counterReducer = (state = 0, action) => {
-    
+    switch (action.type) {
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
+    }
 }; // Define the counter reducer which will increment or decrement the state based on the action it receives.
 
 const incAction = null; // Define an action creator for incrementing 
