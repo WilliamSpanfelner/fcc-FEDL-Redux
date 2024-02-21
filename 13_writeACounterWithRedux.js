f67e90e7280802ab34c61c37d9a6fc81ef3f7ef8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-02-21 17:46:20 +0200

    Assign strings to the action type variables

--- 13_writeACounterWithRedux.js
+++ 13_writeACounterWithRedux.js
@@ -1,6 +1,6 @@
-const INCREMENT = null; // Define a constant for increment action types
+const INCREMENT = 'INCREMENT'; // Define a constant for increment action types
 
-const DECREMENT = null; // Define a constant for increment action types
+const DECREMENT = 'DECREMENT'; // Define a constant for increment action types
 
 const counterReducer = null; // Define the counter reducer which will increment or decrement the state based on the action it receives.
 
